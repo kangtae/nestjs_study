5612e226e599e63fbc9c3a950db5d41bb2bbacdd
kangtaewan <bras1717@naver.com>
2025-10-21 21:50:38 +0900

    get 메서드 만들기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,25 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+/* nest g resource */
+
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
   likeCount: number;
   commentCount: number;
 }
-/* nest g resource */
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjens_official',
+    title: '뉴진스 민지',
+    content: '메이업 고고 있는 민지',
+    likeCount: 100000,
+    commentCount: 99999,
+  },
+  {
+    id: 2,
+    author: 'newjens_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 100000,
+    commentCount: 99999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '아파트 로제',
+    likeCount: 100000,
+    commentCount: 99999,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjens_official',
-      title: '뉴진스 민지',
-      content: '메이업 고고 있는 민지',
-      likeCount: 100000,
-      commentCount: 99999,
-    };
+  getPosts() {
+    return posts;
+  }
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) throw new NotFoundException();
+    return post;
   }
 }
